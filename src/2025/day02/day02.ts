07c9ecd0b4ccafbbc5b5f2088ac6ac767310103a
William Jones <37749088+wdj82@users.noreply.github.com>
2025-12-01 09:43:27 -0600

    2025 day 2 setup

---
+++ src/2025/day02/day02.ts
@@ -0,0 +1,19 @@
+// Advent of Code day 2
+// https://adventofcode.com/2025/day/2
+
+import { rawInput } from "./rawInput";
+
+function solvePartOne() {
+  return 0;
+}
+
+function solvePartTwo() {
+  return 0;
+}
+
+const partOne = solvePartOne();
+const partTwo = solvePartTwo();
+
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+
