9975741c9a1ff6eb5feffe9d32f96df5b16fc7f7
William Jones <37749088+wdj82@users.noreply.github.com>
2025-12-01 09:11:59 -0600

    project setup

---
+++ src/2025/day01/day01.ts
@@ -0,0 +1,11 @@
+// Advent of Code day 1
+// https://adventofcode.com/2025/day/1
+
+import { rawInput } from "./rawInput";
+
+const partOne = 0;
+const partTwo = 0;
+
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+
