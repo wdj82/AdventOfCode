0359877211dca84790bc8a5d1fed24593bc05f3a
William Jones <37749088+wdj82@users.noreply.github.com>
2025-12-01 09:59:44 -0600

    2025 day 3 setup

---
+++ src/2025/day03/day03.ts
@@ -0,0 +1,19 @@
+// Advent of Code day 3
+// https://adventofcode.com/2025/day/3
+
+import { rawInput } from "./rawInput";
+
+function solvePartOne() {
+  return 0;
+}
+
+function solvePartTwo() {
+  return 0;
+}
+
+const partOne = solvePartOne();
+const partTwo = solvePartTwo();
+
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
+
