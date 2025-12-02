c84645a2314e4ba289a438bc04e8e15a57164883
William <wjones@clearc2.com>
2025-12-02 10:34:28 -0600

    2025 day 2 complete

--- src/2025/day02/day02.ts
+++ src/2025/day02/day02.ts
@@ -3,12 +3,41 @@
 
 import { rawInput } from "./rawInput";
 
+const ranges = rawInput.split(",").map((ranges) => ranges.split("-").map(Number));
+
 function solvePartOne() {
-  return 0;
+  let count = 0;
+
+  ranges.forEach(([start, end]) => {
+    for (let i = start; i <= end; i++) {
+      const currentId = i.toString();
+      if (currentId.length % 2 !== 0) continue;
+      const firstHalf = currentId.slice(0, currentId.length / 2);
+      const secondHalf = currentId.slice(currentId.length / 2);
+      if (firstHalf === secondHalf) {
+        count += i;
+      }
+    }
+  });
+
+  return count;
 }
 
 function solvePartTwo() {
-  return 0;
+  let count = 0;
+
+  ranges.forEach(([start, end]) => {
+    for (let i = start; i <= end; i++) {
+      const currentId = i.toString();
+      const doubled = currentId + currentId;
+      const isWrongId = doubled.indexOf(currentId, 1) !== currentId.length;
+      if (isWrongId) {
+        count += i;
+      }
+    }
+  });
+
+  return count;
 }
 
 const partOne = solvePartOne();
