4173aed29ad5360e3b0a8cfdd3080572f869bac1
William Jones <37749088+wdj82@users.noreply.github.com>
2025-12-06 14:50:57 -0600

    2025 day 8 setup

---
+++ src/2025/day08/day08.ts
@@ -0,0 +1,20 @@
+// Advent of Code day 8
+// https://adventofcode.com/2025/day/8
+
+// import { rawInput } from "./rawInput";
+
+const rawInput = ``;
+
+function solvePartOne() {
+  return 0;
+}
+
+function solvePartTwo() {
+  return 0;
+}
+
+const partOne = solvePartOne();
+const partTwo = solvePartTwo();
+
+document.getElementById("partOne")?.appendChild(document.createTextNode(partOne.toString()));
+document.getElementById("partTwo")?.appendChild(document.createTextNode(partTwo.toString()));
